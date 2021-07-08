4a871dd5f36de23562995598b85ade2516aa55f5
Vinayak-VG <vinayakguptapokal@gmail.com>
2021-07-08 19:10:56 +0530

    Scripts

--- Computer Vision Projects/2D Image Segmentation/U-Net Image Segmentation/U-Net++/scripts/train.py
+++ Computer Vision Projects/2D Image Segmentation/U-Net Image Segmentation/U-Net++/scripts/train.py
@@ -22,28 +22,29 @@ from PIL import Image
 import cv2
 from model import *
 from dataloader import *
+from tqdm import trange
 
 path = "/content/drive/MyDrive/U-Net_DataSet"
 batch_size = 1
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-transform = transforms.Compose([transforms.ToTensor(),])
-
+transform = transforms.Compose([transforms.ToTensor()])
+print("Starting to load dataset")
 train_dataset = Dataload("/content/drive/MyDrive/U-Net_Train_Deformed", transform)
 train_set, val_set = train_test_split(train_dataset, test_size=0.2, random_state=42)
 train_dataloader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers = 2)
 val_dataloader = torch.utils.data.DataLoader(val_set, batch_size=batch_size, shuffle=False, num_workers = 2)
 
-test_dataset = Dataload_Test("/content/drive/MyDrive/U-Net_DataSet", transforms)
+test_dataset = Dataload_Test("/content/drive/MyDrive/U-Net_DataSet", transform)
 test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers = 2)
-
+print("Dataset loading done")
 epoch_train_losses = []              # Defining an empty list to store the epoch losses
 epoch_val_losses = []             
 accu_train_epoch = []                # Defining an empty list to store the accuracy per epoch
 accu_val_epoch = []
 
 model = UNetPP(DenseBlock, BasicDownBlock, BasicUpBlock)
-
+print("Model is loaded")
 def make_tensor(tensor):
       if torch.cuda.is_available():
         return torch.cuda.FloatTensor(tensor)
@@ -99,25 +100,23 @@ def train(model, dataset, optimizer, criterion, device):
     accu_train_batch = []
     model.train()
     for idx,(images, labels) in enumerate(dataset):
-      images = images.to(device)
-      labels = labels.to(device)
+        images = images.to(device)
+        labels = labels.to(device)
 
-      #Forward Pass
-      output = model(make_tensor(images))
-    #   print(output)
-    #   print(labels)
-      output = torch.clip(output, 0.0025, 0.9975)            # I am clipping the output because if it becomes 0 or 1 then there is a chance that loss function can explode
-      labels = torch.round(labels)
-      train_loss = criterion(output, labels)
-      train_loss_batch.append(train_loss)
-      output = torch.round(output)
-      acc = iou_score(output, labels)
-      accu_train_batch.append(acc)
-      print(f"Batch: {idx + 1}   Train Loss: {train_loss:.5f}   Accuracy: {acc:.5f}")
-      # Backward
-      optimizer.zero_grad()
-      train_loss.backward()
-      optimizer.step()
+        #Forward Pass
+        output = model(make_tensor(images))
+        output = torch.clip(output, 0.0025, 0.9975)            # I am clipping the output because if it becomes 0 or 1 then there is a chance that loss function can explode
+        labels = torch.round(labels)
+        train_loss = criterion(output, labels)
+        train_loss_batch.append(train_loss)
+        output = torch.round(output)
+        acc = iou_score(output, labels)
+        accu_train_batch.append(acc)
+        print(f"Batch: {idx + 1}   Train Loss: {train_loss:.5f}   Accuracy: {acc:.5f}")
+        # Backward
+        optimizer.zero_grad()
+        train_loss.backward()
+        optimizer.step()
     epoch_train_losses.append(sum(train_loss_batch)/360)
     accu_train_epoch.append(sum(accu_train_batch)/360)
     print(f"Train Epoch Loss: {(sum(train_loss_batch)/360):.5f}   Train Epoch Accuracy: {(sum(accu_train_batch)/360):.5f}")
@@ -128,18 +127,18 @@ def eval(model, dataset, criterion, device):
     accu_val_batch = []
     model.eval()
     for idx,(images, labels) in enumerate(dataset):
-      with torch.no_grad():
-        images = images.to(device)
-        labels = labels.to(device)
-        #Forward Pass
-        output = model(make_tensor(images))
-        torch.clip(output, 0.0025, 0.9975)
-        # Loss
-        val_loss = criterion(output, labels)
-        val_loss_batch.append(val_loss)
-        output = torch.round(output)
-        acc = iou_score(output, labels)
-        accu_val_batch.append(acc)
+        with torch.no_grad():
+            images = images.to(device)
+            labels = labels.to(device)
+            #Forward Pass
+            output = model(make_tensor(images))
+            torch.clip(output, 0.0025, 0.9975)
+            # Loss
+            val_loss = criterion(output, labels)
+            val_loss_batch.append(val_loss)
+            output = torch.round(output)
+            acc = iou_score(output, labels)
+            accu_val_batch.append(acc)
     epoch_val_losses.append((sum(val_loss_batch))/90)
     accu_val_epoch.append((sum(accu_val_batch))/90)
     print(f"Val Epoch Loss: {((sum(val_loss_batch))/90):.5f}   Val Epoch Accuracy: {((sum(accu_val_batch))/90):.5f}")
@@ -152,16 +151,16 @@ def epoch_time(start_time, end_time):
     elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
     return elapsed_mins, elapsed_secs
 
-for epoch in range(epochs):
+for epoch in trange(epochs):
     
     start_time = time.monotonic()
 
     print(f"Epoch: {epoch + 1}")
     train(model, train_dataloader, optimizer, loss, device)
     eval(model, val_dataloader, loss, device)
-    if best_valid_loss > epoch_val_losses[-1]:
-      best_valid_loss = epoch_val_losses[-1]
-      torch.save({
+    # if best_valid_loss > epoch_val_losses[-1]:
+    #     best_valid_loss = epoch_val_losses[-1]
+    torch.save({
             'epoch': epoch,
             'model_state_dict': model.state_dict(),
             'optimizer_state_dict': optimizer.state_dict(),
